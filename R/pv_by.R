8cba71201c84ef208d84ecf890dde67f6776645d
Manuel Reif <manuel.reif@statistik.gv.at>
2024-05-03 09:44:38 +0200

    Umlaut problems fixed regular expressions

--- R/pv_by.R
+++ R/pv_by.R
@@ -1,23 +1,23 @@
 pv_by <-
 function(by, svydat, pvs, FUNC)
 {
-  
+
   avar <- c(pvs, all.vars(by))
-  
+
   dum1 <- apply(svydat$variables[,avar], 1, function(x) any(is.na(x)))
   svydat.sub <- subset(svydat, dum1 == FALSE)
   #svydat.sub <- svydat
-  
-  # um nachher die richtige anzahl an spalten rausnehmen zu können (ergmat)
+
+  # um nachher die richtige anzahl an spalten rausnehmen zu koennen (ergmat)
   howmanyby <- 1:length(all.vars(by))
-  
+
   ## Hier werden die PV durchgerattert
   rawerg <- lapply(pvs, function(p)
       {
       FORM <- paste("~",p,collapse=" ")
-      
-      erg1 <- svyby(formula = eval(parse(text=FORM)), 
-                    by= by, 
+
+      erg1 <- svyby(formula = eval(parse(text=FORM)),
+                    by= by,
                     svydat, FUN=eval(parse(text=FUNC)),
                     na.rm=TRUE, na.rm.by=TRUE)
       return(erg1)
@@ -26,25 +26,25 @@ function(by, svydat, pvs, FUNC)
   # nimm nur die numeric variables raus
   ergmat <- lapply(rawerg, function(x)
               {
-                u1 <- as.matrix(x[,-howmanyby]) 
+                u1 <- as.matrix(x[,-howmanyby])
                 zw1 <- u1[,"se"]^2
                 cbind(u1, varvse = zw1)
               })
-              
+
 
   # mach ein 3d array mit den numerischen variablen
   dreid <- simplify2array(ergmat)
-  
-  
+
+
   # summiere ueber die 3.dimension also ?ber die replizierten 10 PVs
   meanit <- apply(dreid,1:2,function(x)
               {
-               mean(x)  
+               mean(x)
               })
-              
+
   # bilde die var() ?ber die PVs replizierten statistiken
   sdit <- apply(dreid,1:2,var)[,1]
-  
+
   # dranhaengen um nachher geschickt transform machen zu k?nnen
   meanit2 <- cbind(meanit,varrep=sdit)
   #cat("hier bin ich")
@@ -53,40 +53,40 @@ function(by, svydat, pvs, FUNC)
 
   meanit3 <- cbind(meanit2,se_end)
   endergv  <- meanit3[,c(1,ncol(meanit3))]
-  
+
   if(dim(dreid)[1] == 1)
         {
-        names(endergv) <- c("mean", "SE")  
+        names(endergv) <- c("mean", "SE")
         } else {
                colnames(endergv) <- c("mean", "SE")
                }
-  
- 
+
+
   #########################################################
   ########### additional information ######################
-  ######################################################### 
+  #########################################################
   # country ID # kommt erst in der user-main-function dazu!
   # N of cases
   # sum of SPFWT0
   # nimm: svydat.sub als grundlage, sonst gibts probleme
   # wenn fehlende werte in den NA's sind
-  
-  
+
+
   ADC <- additional_comp(by=by,svydat=svydat.sub)
-  
-  
+
+
   if(dim(dreid)[1] == 1)
   {
-  
-    
+
+
   endergvM <- matrix(endergv,ncol=2,dimnames=list(1,names(endergv)))
-  enderg   <- data.frame("Group1"=unique(svydat$variables[,all.vars(by)]),endergvM)  
-    
-    
-    
-    
+  enderg   <- data.frame("Group1"=unique(svydat$variables[,all.vars(by)]),endergvM)
+
+
+
+
   } else {
-    
+
 
     # necessary because the order of by-variable combination is different between the functions
     # ------------------------------------------------------------------
@@ -94,15 +94,15 @@ function(by, svydat, pvs, FUNC)
     splitndf   <- data.frame(matrix(unlist(splitnames),ncol=length(splitnames[[1]]), byrow=TRUE))
     colnames(splitndf) <- paste0("Group",1:length(all.vars(by)))
     enderg             <- data.frame(splitndf,endergv)
-    rownames(enderg)   <- NULL    
- 
+    rownames(enderg)   <- NULL
+
   }
-  
+
   colnames(ADC$Ncases)     <- c(paste0("Group",1:length(all.vars(by))),"Number.of.cases")
   colnames(ADC$Sumweights) <- c(paste0("Group",1:length(all.vars(by))),"Sum.of.weights")
-  
-  
-  
+
+
+
   return(list(enderg=enderg, Ncases=ADC$Ncases, Sumweights=ADC$Sumweights))
-  
+
 }
--- R/pv_perc.R
+++ R/pv_perc.R
@@ -5,36 +5,36 @@ function(by, svydat, pvcat)
 # by     =  wonach kategorisiert werden soll
 # svydat =  svyrepdesign
 
-  
+
   #################################
   #### check ######################
 
   # hier wird ?berpr?ft ob eh alle PV-variablen 'factors' sind.
   ISFACTOR<- sapply(svydat$variables[,names(svydat$variables) %in% pvcat],is.factor)
   if(!all(ISFACTOR)){stop("Check whether all the variables provided with the 'by' statement are of class factor.")}
-  
-  
+
+
   ################################
-  
+
   howmanyby <- 1:length(all.vars(by))
-  
+
   # hier werden die zellen% berechnet inkl. SE.
   ## das funktioniert indem man svymean als funktion nimmt, und als formula einen factor!
-  
+
   rawerg <- lapply(pvcat, function(p)
                 {
                   FORM <- paste("~",p,collapse=" ")
-                  erg1 <- svyby(formula = eval(parse(text=FORM)), 
-                                by= by, 
-                                design=svydat, FUN=svymean, na.rm=TRUE)  
+                  erg1 <- svyby(formula = eval(parse(text=FORM)),
+                                by= by,
+                                design=svydat, FUN=svymean, na.rm=TRUE)
                   return(erg1)
                 })
-  
-  
+
+
   # nimm nur die numeric variables raus
   ergmat <- lapply(rawerg, function(x)
               {
-                #u1   <- as.matrix(x[,sapply(x,is.numeric)]) 
+                #u1   <- as.matrix(x[,sapply(x,is.numeric)])
                 u1   <- as.matrix(x[,-howmanyby])
                 wose <- grep("^se\\d*$",colnames(u1),perl=TRUE)
                 # rbind garantiert mir, dass auch bei nur einer kategorie das ganze ein zeilenvector ist!
@@ -42,26 +42,26 @@ function(by, svydat, pvcat)
                 colnames(zw1) <- paste0("varvse",1:ncol(zw1))
                 cbind(u1, zw1)
               })
-  
-  
+
+
   # mach ein 3d array mit den numerischen variablen
-  dreid <- simplify2array(ergmat)  
-  
-  
+  dreid <- simplify2array(ergmat)
+
+
   # bilde den Mittelwert ?ber die 3.dimension also ?ber die replizierten 10 PVs
   meanit <- apply(dreid,1:2,function(x)
               {
-                mean(x)  
+                mean(x)
               })
-  
-  
+
+
   # bilde die var() ?ber die PVs replizierten statistiken
   sdita <- apply(dreid,1:2,var)
   wose  <- grep("^(se|varvse)\\d*$",colnames(sdita),perl=TRUE)
   sdit  <- rbind(sdita[,-wose]) # nimmt nur die means raus (weil mich nur die VAR der SE interessiert)
   colnames(sdit) <- paste0("varrep",1:ncol(sdit))
-              
-  
+
+
   # nimmt hier jetzt nur die mittlere varianz raus
   VARV <- rbind(meanit[,grep("^varvse\\d*$",colnames(meanit))])
   # endberechnung
@@ -70,14 +70,14 @@ function(by, svydat, pvcat)
   endergt1  <- rbind(meanit[,-wose])
   #colnames(endergt1) <- gsub(pvcat[1],"Percent ",colnames(endergt1))
   colnames(endergt1) <- gsub(pvcat[1],"",colnames(endergt1))
-  
+
   colnames(SEend) <- gsub("Percent ","SE ", colnames(endergt1))
-  
+
   # fertig
   endergv  <- cbind(endergt1,SEend)
-  
 
-  
+
+
   if(dim(dreid)[1] == 1)
     {
       grnam    <- unique(svydat$variables[,all.vars(by)])
@@ -85,7 +85,7 @@ function(by, svydat, pvcat)
       enderg   <- data.frame(splitndf,endergv)
       colnames(enderg)   <- c("Group1",colnames(endergv))
       rownames(enderg)   <- NULL
-      
+
     } else {
             # necessary because the order of by-variable combination is different between the functions
             # ------------------------------------------------------------------
@@ -95,47 +95,47 @@ function(by, svydat, pvcat)
             # colnames(splitndf) <- paste0("Group",1:length(all.vars(by)))
             enderg             <- data.frame(splitndf,endergv)
             colnames(enderg)   <- c(paste0("Group",1:length(all.vars(by))),colnames(endergv))
-            rownames(enderg)   <- NULL    
+            rownames(enderg)   <- NULL
             }
-  
 
-  
+
+
   endnum <- enderg[ ,-(1:length(all.vars(by)))]
   numbCAT <- ncol(endnum)/2 # number of categories
   endnam <- enderg[rep(1:nrow(enderg),each=numbCAT) , (1:length(all.vars(by)))]
-  
+
   eins <- apply(endnum,1,function(x) list(mymat=matrix(x,nrow=numbCAT)))
   zwei <- lapply(eins,function(A)A[[1]])
   drei <- do.call("rbind",zwei)
   colnames(drei) <- c("Proportion","Proportion:SE")
-  
+
   CN <- colnames(enderg)[-howmanyby][1:numbCAT]
-  
+
   endergF <- data.frame(endnam, CN , drei)
   colnames(endergF)[1:(length(howmanyby)+1)] <- paste0("Group",1:(length(howmanyby)+1))
   rownames(endergF) <- NULL
-  
-  
+
+
   #########################################################
   ########### additional information ######################
-  ######################################################### 
+  #########################################################
   # country ID # kommt erst in der user-main-function dazu!
   # N of cases
   # sum of SPFWT0
   # nimm: svydat.sub als grundlage, sonst gibts probleme
   # wenn fehlende werte in den NA's sind
-  
-  
-  # hier sollte man sich überlegen ob man die zusatzinfos wirklich will, und wenn ja wie.
-  # denn die ANZAHL DER PERSONEN JE GRUPPE ist nichtmehr so einfach zu bestimmen, weil die Gruppen ja 
+
+
+  # hier sollte man sich ueberlegen ob man die zusatzinfos wirklich will, und wenn ja wie.
+  # denn die ANZAHL DER PERSONEN JE GRUPPE ist nichtmehr so einfach zu bestimmen, weil die Gruppen ja
   # AUCH plausible values sind!!!
-  
-  
+
+
   ADC <- additional_compPV(by=by, svydat=svydat, pvcat=pvcat)
-  
+
   colnames(ADC$Ncases)     <- c(paste0("Group",1:(length(howmanyby)+1)),"Number.of.cases")
   colnames(ADC$Sumweights) <- c(paste0("Group",1:(length(howmanyby)+1)),"Sum.of.weights")
-  
+
 
   return(list(endergF=endergF, Ncases=ADC$Ncases, Sumweights=ADC$Sumweights))
 
