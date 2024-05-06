e99accf7597cc3316244d23a4e60caea04f86b2a
Manuel Reif <manuel.reif@statistik.gv.at>
2024-05-06 14:19:49 +0200

    debugged error in R >= 4.3

--- R/REG_AUX.R
+++ R/REG_AUX.R
@@ -2,27 +2,47 @@
 # REG AUX FUNCTIONS
 ########################################################################
 
-glm.dummy.no.std <- function(formula, 
-                             design_dummy_4_glm.dummy.no.std,
-                             family_dummy_4_glm.dummy.no.std){
-  design_dummy_4_glm.dummy.no.std <<- design_dummy_4_glm.dummy.no.std
-  family_dummy_4_glm.dummy.no.std <<- family_dummy_4_glm.dummy.no.std
+glm.dummy.no.std <- function(formula,
+                             design,
+                             family){
   # estimate mod
-  mod <- svyglm(formula, 
-                design_dummy_4_glm.dummy.no.std, 
-                family=family_dummy_4_glm.dummy.no.std)
+  mod <- svyglm(formula,
+                design = design,
+                family = family)
   res <- summary(mod)
   # goodness of fit
   test.term <- as.character(formula[3])
   #.e <- environment()
   test <- regTermTest(mod, as.formula(paste("~", test.term)),
                       method="LRT")
-  rm(list=c("design_dummy_4_glm.dummy.no.std", 
-            "family_dummy_4_glm.dummy.no.std"),
-     envir = .GlobalEnv)
-  return(list(coef = res$coef, 
+  # rm(list=c("design_dummy_4_glm.dummy.no.std",
+  #           "family_dummy_4_glm.dummy.no.std"),
+  #    envir = .GlobalEnv)
+  return(list(coef = res$coef,
               mod.fit = cbind(test$chi, t(test$lambda), test$ddf)))
 }
 
 
 
+#
+# glm.dummy.no.std_OLD <- function(formula,
+#                              design_dummy_4_glm.dummy.no.std,
+#                              family_dummy_4_glm.dummy.no.std){
+#   design_dummy_4_glm.dummy.no.std <<- design_dummy_4_glm.dummy.no.std
+#   family_dummy_4_glm.dummy.no.std <<- family_dummy_4_glm.dummy.no.std
+#   # estimate mod
+#   mod <- svyglm(formula,
+#                 design_dummy_4_glm.dummy.no.std,
+#                 family=family_dummy_4_glm.dummy.no.std)
+#   res <- summary(mod)
+#   # goodness of fit
+#   test.term <- as.character(formula[3])
+#   #.e <- environment()
+#   test <- regTermTest(mod, as.formula(paste("~", test.term)),
+#                       method="LRT")
+#   rm(list=c("design_dummy_4_glm.dummy.no.std",
+#             "family_dummy_4_glm.dummy.no.std"),
+#      envir = .GlobalEnv)
+#   return(list(coef = res$coef,
+#               mod.fit = cbind(test$chi, t(test$lambda), test$ddf)))
+# }
--- R/REG_MAIN.R
+++ R/REG_MAIN.R
@@ -1,29 +1,29 @@
 ########################################################################
 # REG MAIN FUNCTIONS
 ########################################################################
-svyPVglm <- function(formula, 
-                     design, 
-                     placeholder = 1:10, 
+svyPVglm <- function(formula,
+                     design,
+                     placeholder = 1:10,
                      family = gaussian()){
   if(length(grep("\\.\\.",formula)) == 0){stop("NO PVS")}
   else{
     as.char.forms <- lapply(placeholder,
                             function(x) gsub("\\.\\.", x, formula))
-    char.calls <-  lapply(as.char.forms, 
+    char.calls <-  lapply(as.char.forms,
                           function(x)(paste(x[2],x[1], x[3])))
     formulas <- lapply(char.calls, as.formula)
-      mods <- lapply(lapply(formulas,as.formula), 
-                     glm.dummy.no.std, 
+      mods <- lapply(formulas,
+                     glm.dummy.no.std,
                      design, family)
       COEF.TAB <- lapply(mods, function(x)(x$coef))
-      C.est <- do.call(rbind, 
-                       lapply(COEF.TAB, 
+      C.est <- do.call(rbind,
+                       lapply(COEF.TAB,
                               function(x)
                                 (t(as.data.frame(x)[1]))
                        )
       )
-      C.sd <- do.call(rbind, 
-                      lapply(COEF.TAB, 
+      C.sd <- do.call(rbind,
+                      lapply(COEF.TAB,
                              function(x)
                                (t(as.data.frame(x)[2]))
                       )
@@ -31,19 +31,19 @@ svyPVglm <- function(formula,
       C.var <- C.sd^2
       Means <- apply(C.est, 2, mean)
       sampling.v <- apply(C.var, 2, mean)
-      imputation.v <- apply(C.est,2, var) 
+      imputation.v <- apply(C.est,2, var)
       tot.se <- sqrt(sampling.v + (1.1 * imputation.v))
       t.value <- Means/tot.se
       t.value.4.test <- abs(t.value)
       Pr.t <- pt(t.value.4.test , design$degf, lower.tail = FALSE)*2
-      
-      COEF.wo.Pr.t <- round(rbind(Means, tot.se, t.value), 
+
+      COEF.wo.Pr.t <- round(rbind(Means, tot.se, t.value),
                             digits = getOption("digits"))
       COEF <- rbind(COEF.wo.Pr.t, Pr.t)
-      rownames(COEF) <- c("mean", "se", "t.value", "Pr.t") 
+      rownames(COEF) <- c("mean", "se", "t.value", "Pr.t")
       FIT.TAB <- lapply(mods, function(x)(x$mod.fit))
-      FIT.est <- t(do.call(cbind, 
-                           lapply(FIT.TAB, 
+      FIT.est <- t(do.call(cbind,
+                           lapply(FIT.TAB,
                                   function(x)
                                     (t(as.data.frame(x)))
                            )
@@ -57,7 +57,7 @@ svyPVglm <- function(formula,
                        lower.tail=FALSE)
       p <- format.pval(p.dummy)
       FIT.return <- data.frame(Working.2.logLR, p)
-      rownames(FIT.return) <- ""    
+      rownames(FIT.return) <- ""
       t.COEF <- as.data.frame(t(COEF))
       t.COEF$Pr.t <- format.pval(t.COEF$Pr.t)
       FINAL.RES <- list(coef=t.COEF, mod.fit=FIT.return)
